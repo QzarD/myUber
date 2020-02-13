9e00e79f73e0c8824f99f28a912c8b422fbeb85d
QzarD <a---angel@mail.ru>
2020-02-13 16:56:16 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDPnjplAkZ9YfbE8WW7RqVxRXMRbTsfXzc",
+    authDomain: "socialclubapp-1597b.firebaseapp.com",
+    databaseURL: "https://socialclubapp-1597b.firebaseio.com",
+    projectId: "socialclubapp-1597b",
+    storageBucket: "socialclubapp-1597b.appspot.com",
+    messagingSenderId: "741967739341",
+    appId: "1:741967739341:web:01d9267245a131100d2c84"
+};
+/*
+export const firebaseConfig = {
+    apiKey: "AIzaSyDHZ6a_6K_ZCO5K351Yg-Bk279HZrQoECU",
+    authDomain: "socialclub-e188d.firebaseapp.com",
+    databaseURL: "https://socialclub-e188d.firebaseio.com",
+    projectId: "socialclub-e188d",
+    storageBucket: "socialclub-e188d.appspot.com",
+    messagingSenderId: "34217809878",
+    appId: "1:34217809878:web:34c9fd142b7fdd2def3677",
+    measurementId: "G-28N5X9D0RV"
+};*/
